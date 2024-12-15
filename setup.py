87d1c298605a552dd3acd5396f9f53cad587d7e4
FadelAlsaleh <Fadel.Alsaleh@outlook.com>
2024-12-15 20:58:03 +0300

    install requirment library

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="FadelAlsaleh",
+    author_email="fadel.alsaleh@outlook",
+    packages=find_packages()
+)
